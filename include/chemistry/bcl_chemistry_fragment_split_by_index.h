18743b789f3593fe1cbe41ad0a869369c81f7b4c
Benjamin Brown <benjamin.p.brown17@gmail.com>
2022-07-07 16:12:27 -0500

    Adding another FragmentSplitInterface-derived class to make it easier to prepare molecules for input to FLD or Mutate.

---
+++ include/chemistry/bcl_chemistry_fragment_split_by_index.h
@@ -0,0 +1,130 @@
+// (c) Copyright BCL @ Vanderbilt University 2014
+// (c) BCL Homepage: http://www.meilerlab.org/bclcommons
+// (c) BCL Code Repository: https://github.com/BCLCommons/bcl
+// (c)
+// (c) The BioChemical Library (BCL) was originally developed by contributing members of the Meiler Lab @ Vanderbilt University.
+// (c)
+// (c) The BCL is now made available as an open-source software package distributed under the permissive MIT license,
+// (c) developed and maintained by the Meiler Lab at Vanderbilt University and contributing members of the BCL Commons.
+// (c)
+// (c) External code contributions to the BCL are welcome. Please visit the BCL Commons GitHub page for information on how you can contribute.
+// (c)
+// (c) This file is part of the BCL software suite and is made available under the MIT license.
+// (c)
+
+#ifndef BCL_CHEMISTRY_FRAGMENT_SPLIT_BY_INDEX_H_
+#define BCL_CHEMISTRY_FRAGMENT_SPLIT_BY_INDEX_H_
+
+// include the namespace header
+#include "bcl_chemistry.h"
+
+// include other forward headers - sorted alphabetically
+
+// includes from bcl - sorted alphabetically
+#include "bcl_chemistry_fragment_split_interface.h"
+#include "util/bcl_util_enumerated.h"
+
+namespace bcl
+{
+  namespace chemistry
+  {
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //!
+    //! @class FragmentSplitByIndex
+    //! @brief Splits molecules into the largest common substructure they possess relative to molecules of an input file
+    //!
+    //! @see @link example_chemistry_fragment_split_largest_common_substructure.cpp @endlink
+    //! @author ben
+    //! @date Jul 07, 2022
+    //!
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    class BCL_API FragmentSplitByIndex :
+      public FragmentSplitInterface
+    {
+
+    private:
+
+    //////////
+    // data //
+    //////////
+
+      //! the atom indices to remove from the input molecules
+      storage::Vector< size_t> m_AtomIndices;
+
+      //! invert the atom index selection prior to removal
+      bool m_Invert;
+
+      //! break a fragment complex into isolated fragments
+      bool m_Break;
+
+    public:
+
+      //! single instance of that class
+      static const util::SiPtr< const util::ObjectInterface> s_Instance;
+
+    //////////////////////////////////
+    // construction and destruction //
+    //////////////////////////////////
+
+      //! virtual copy constructor
+      FragmentSplitByIndex *Clone() const;
+
+      //! @brief constructor
+      FragmentSplitByIndex
+      (
+        const storage::Vector< size_t> &ATOM_INDICES = storage::Vector< size_t>(),
+        const bool INVERT = false,
+        const bool BREAK = false
+      );
+
+    /////////////////
+    // data access //
+    /////////////////
+
+      //! @brief returns class name
+      //! @return the class name as const ref std::string
+      const std::string &GetClassIdentifier() const;
+
+      //! @brief returns the name used for this class in an object data label
+      //! @return the name used for this class in an object data label
+      const std::string &GetAlias() const;
+
+      //! @brief Get a description for what this class does (used when writing help)
+      //! @return a description for what this class does (used when writing help)
+      const std::string &GetClassDescription() const;
+
+    ////////////////
+    // operations //
+    ////////////////
+
+      //! @brief returns an ensemble of fragments of a molecule
+      //! @param CONFORMATION molecule of interest
+      //! @return an ensemble of common substructures relative to those in a file
+      FragmentEnsemble operator()( const ConformationInterface &CONFORMATION) const;
+
+    //////////////////////
+    // helper functions //
+    //////////////////////
+
+      //! @brief reads in molecules from a given file if it is necessary
+      void ReadFile() const;
+
+    protected:
+
+      //! @brief Set the members of this property from the given LABEL
+      //! @param LABEL the label to parse
+      //! @param ERR_STREAM stream to write out errors to
+      bool ReadInitializerSuccessHook( const util::ObjectDataLabel &LABEL, std::ostream &ERR_STREAM);
+
+      //! @brief return parameters for member data that are set up from the labels
+      //! @return parameters for member data that are set up from the labels
+      io::Serializer GetSerializer() const;
+
+    };
+
+  } // namespace chemistry
+} // namespace bcl
+
+#endif // BCL_CHEMISTRY_FRAGMENT_SPLIT_BY_INDEX_H_
---
+++ source/chemistry/bcl_chemistry_fragment_split_by_index.cpp
@@ -0,0 +1,202 @@
+// (c) Copyright BCL @ Vanderbilt University 2014
+// (c) BCL Homepage: http://www.meilerlab.org/bclcommons
+// (c) BCL Code Repository: https://github.com/BCLCommons/bcl
+// (c)
+// (c) The BioChemical Library (BCL) was originally developed by contributing members of the Meiler Lab @ Vanderbilt University.
+// (c)
+// (c) The BCL is now made available as an open-source software package distributed under the permissive MIT license,
+// (c) developed and maintained by the Meiler Lab at Vanderbilt University and contributing members of the BCL Commons.
+// (c)
+// (c) External code contributions to the BCL are welcome. Please visit the BCL Commons GitHub page for information on how you can contribute.
+// (c)
+// (c) This file is part of the BCL software suite and is made available under the MIT license.
+// (c)
+
+// initialize the static initialization fiasco finder, if macro ENABLE_FIASCO_FINDER is defined
+#include "util/bcl_util_static_initialization_fiasco_finder.h"
+BCL_StaticInitializationFiascoFinder
+
+// include header of this class
+#include "chemistry/bcl_chemistry_fragment_split_by_index.h"
+
+// includes from bcl - sorted alphabetically
+#include "chemistry/bcl_chemistry_fragment_complete.h"
+#include "chemistry/bcl_chemistry_fragment_split_isolate.h"
+#include "io/bcl_io_file.h"
+
+// external includes - sorted alphabetically
+
+namespace bcl
+{
+  namespace chemistry
+  {
+
+    // add the interface to the set of known implementations
+    const util::SiPtr< const util::ObjectInterface> FragmentSplitByIndex::s_Instance
+    (
+      util::Enumerated< FragmentSplitInterface>::AddInstance( new FragmentSplitByIndex)
+    );
+
+  //////////////////////////////////
+  // construction and destruction //
+  //////////////////////////////////
+
+    //! @brief constructor, sets default steps to 4
+    FragmentSplitByIndex::FragmentSplitByIndex
+    (
+      const storage::Vector< size_t> &ATOM_INDICES,
+      const bool INVERT,
+      const bool BREAK
+    ) :
+      m_AtomIndices( ATOM_INDICES),
+      m_Invert( INVERT),
+      m_Break( BREAK)
+    {
+    }
+
+    //! virtual copy constructor
+    FragmentSplitByIndex *FragmentSplitByIndex::Clone() const
+    {
+      return new FragmentSplitByIndex( *this);
+    }
+
+  /////////////////
+  // data access //
+  /////////////////
+
+    //! @brief returns class name
+    //! the class name as const ref std::string
+    const std::string &FragmentSplitByIndex::GetClassIdentifier() const
+    {
+      return GetStaticClassName( *this);
+    }
+
+    //! @brief get a short name for this class
+    //! @return a short name for this class
+    const std::string &FragmentSplitByIndex::GetAlias() const
+    {
+      static const std::string s_name( "Index");
+      return s_name;
+    }
+
+    //! @brief Get a description for what this class does (used when writing help)
+    //! @return a description for what this class does (used when writing help)
+    const std::string &FragmentSplitByIndex::GetClassDescription() const
+    {
+      return GetStaticClassName( *this);
+    }
+
+    //! @brief gets the minimum size of fragments
+    //! @return the minimum size of fragments
+    const size_t FragmentSplitByIndex::GetMinSize() const
+    {
+      return 0;
+    }
+
+  /////////////////
+  //  operations //
+  /////////////////
+
+    //! @brief returns an ensemble of fragments of a molecule
+    //! @param CONFORMATION molecule of interest
+    //! @return an ensemble of common substructures relative to those in a file
+    //! TODO: Implement this
+    FragmentEnsemble FragmentSplitByIndex::operator()( const ConformationInterface &CONFORMATION) const
+    {
+      // we will want to construct an atom vector to build our return ensemble
+      storage::Vector< sdf::AtomInfo> atominfo( CONFORMATION.GetAtomInfo());
+      storage::Vector< sdf::BondInfo> bondinfo( CONFORMATION.GetBondInfo());
+      AtomVector< AtomComplete> atoms( atominfo, bondinfo);
+
+      // invert atom indices if desired
+      storage::Vector< size_t> keep_indices;
+      if( m_Invert)
+      {
+        keep_indices = m_AtomIndices;
+      }
+      else
+      {
+        // each atom in original conformation
+        for( size_t i( 0); i < CONFORMATION.GetSize(); ++i)
+        {
+          // if not found in removal indices
+          if( m_AtomIndices.Find( i) >= m_AtomIndices.GetSize())
+          {
+            keep_indices.PushBack( i);
+          }
+        }
+      }
+
+      // remove atoms except those we designated to save
+      atoms.Reorder( keep_indices);
+
+      // build a new molecule
+      FragmentComplete mol( atoms, CONFORMATION.GetName());
+
+      // break if desired
+      if( m_Break)
+      {
+        // accept fragments as small as 1 atom
+        FragmentSplitIsolate isolater( 1);
+
+        // return the isolated ensemble
+        return isolater( mol);
+      }
+
+      return FragmentEnsemble( storage::List< FragmentComplete>( 1, mol));
+    }
+
+    //! @brief Set the members of this property from the given LABEL
+    //! @param LABEL the label to parse
+    //! @param ERR_STREAM stream to write out errors to
+    bool FragmentSplitByIndex::ReadInitializerSuccessHook
+    (
+      const util::ObjectDataLabel &LABEL,
+      std::ostream &ERR_STREAM
+    )
+    {
+      if( m_AtomIndices.GetSize())
+      {
+        return true;
+      }
+
+      BCL_MessageStd("No atom indices provided! Exiting without splitting molecules.");
+      return false;
+    }
+
+    //! @brief return parameters for member data that are set up from the labels
+    //! @return parameters for member data that are set up from the labels
+    io::Serializer FragmentSplitByIndex::GetSerializer() const
+    {
+      io::Serializer parameters;
+      parameters.SetClassDescription
+      (
+        "splits molecules into fragments by removing specified indices"
+      );
+      parameters.AddInitializer
+      (
+        "atom_indices",
+        "the 0-indexed atom indices to remove from the input molecules",
+        io::Serialization::GetAgent( &m_AtomIndices)
+      );
+      parameters.AddInitializer
+      (
+        "invert",
+        "invert the atom index selection prior to atom removal",
+        io::Serialization::GetAgent( &m_Invert),
+        "false"
+      );
+      parameters.AddInitializer
+      (
+        "break",
+        "if removing an atom separates a molecule into isolated components, return those isolated "
+        "components as separate fragments; by default, a fragment complex is returned in such cases.",
+        io::Serialization::GetAgent( &m_Break),
+        "false"
+      );
+
+      return parameters;
+    }
+
+  } // namespace chemistry
+} // namespace bcl
